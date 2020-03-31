2626faf0c22b745498f9b262be240550840c6655
Michal Dzialek <exodusofdead@gmail.com>
2020-03-31 16:59:54 +0200

    Updated API keys

--- photozator/src/reducers/rootReducer.js
+++ photozator/src/reducers/rootReducer.js
@@ -1,9 +1,13 @@
 /* Main reducer, because there will be only one data updated in redux, there is no need for more reducers */
 
-import REACT_APP_RAPIX_X_API_KEY from '../Key.js';
+import REACT_APP_OCR_API_KEY from '../Key.js';
+import REACT_APP_RAPID_API_KEY from '../Key.js';
+import REACT_APP_YANDEX_API_KEY from '../Key.js';
 
 const initState = {
-    Key: REACT_APP_RAPIX_X_API_KEY,
+    OCR_Key: REACT_APP_OCR_API_KEY,
+    RAPIDAPI_Key: REACT_APP_RAPID_API_KEY,
+    YANDEX_Key: REACT_APP_YANDEX_API_KEY,
     ImageText: "",
     TranslatedText: ""
 }
