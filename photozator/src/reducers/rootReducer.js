2e5da65bd32fe3fc953ebbedf49fec1548eb0ec3
Michal Dzialek <exodusofdead@gmail.com>
2020-04-01 20:01:18 +0200

    Updated reducers

--- photozator/src/reducers/rootReducer.js
+++ photozator/src/reducers/rootReducer.js
@@ -1,8 +1,6 @@
 /* Main reducer, because there will be only one data updated in redux, there is no need for more reducers */
 
-import REACT_APP_OCR_API_KEY from '../Key.js';
-import REACT_APP_RAPID_API_KEY from '../Key.js';
-import REACT_APP_YANDEX_API_KEY from '../Key.js';
+import { REACT_APP_OCR_API_KEY, REACT_APP_RAPID_API_KEY, REACT_APP_YANDEX_API_KEY } from '../Key.js';
 
 const initState = {
     OCR_Key: REACT_APP_OCR_API_KEY,
@@ -12,7 +10,25 @@ const initState = {
     TranslatedText: ""
 }
 
-function rootReducer(state = initState, action){}
+function rootReducer(state = initState, action){
+    if(action.type == 'UPDATE_IMAGETEXT'){
+        return{
+            ...state,
+            ImageText: action.ImageText
+        }
+    } else if(action.type == 'UPDATE_TRANSLATEDTEXT'){
+        return{
+            ...state,
+            TranslatedText: action.TranslatedText
+        }
+    } else if(action.type == 'RESET_TEXT_FIELDS'){
+        return{
+            ...state,
+            ImageText: "",
+            TranslatedText: ""
+        }
+    }
+}
 
 
 export default rootReducer;
