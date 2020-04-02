d5ce5204a7d943b1f165161cff567ccb86f8aee6
Michal Dzialek <exodusofdead@gmail.com>
2020-04-02 17:46:09 +0200

    Added Thunk

---
+++ photozator/src/components/photo/Photo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import updateImageText from '../../actions/UpdateActions';
+
+class Photo extends Component {
+    render(){
+
+    }
+}
+
+
+export default connect()(Photo);
--- photozator/src/index.js
+++ photozator/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer'
+import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer); //Create redux store
+const store = createStore(rootReducer, applyMiddleware(thunk)); //Create redux store
 
 ReactDOM.render(
   <React.StrictMode>
